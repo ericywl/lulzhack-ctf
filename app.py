2237c29a3293dbef240948ab3b2d3672461e1203
Eric Yap <22215921+ericywl@users.noreply.github.com>
2019-04-11 14:13:16 +0800

    Change some stuff in app.py

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ application = Flask(__name__)
 @application.route('/')
 def welcome():
     return render_template('index.html')
+    
+@application.route('/signin')
+def signin():
+    return render_template('signin.html')
+    
+@application.errorhandler(404)
+def page_not_found(err):
+    return render_template('error.html'), 404
 
 
 if __name__ == "__main__":
